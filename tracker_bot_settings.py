ead02a323a9e7ecf2c29e486730fa42e26c42d9f
Liam Ralph <liamralph42@gmail.com>
2026-02-21 15:18:09 -0500

    readd settings

---
+++ tracker_bot_settings.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2026 Liam Ralph
+# https://github.com/liam-ralph
+
+# This program, including this file, is licensed under the
+# MIT (Expat) License.
+# See LICENSE or this project's source for more information.
+# Project source: https://github.com/liam-ralph/discord-tracker-bot
+
+# Discord Tracker Bot, a simple Discord bot that pings a
+# user and trackers their responses.
+
+
+# Imports
+
+import datetime
+
+
+# Static Variables
+
+# Discord Token
+
+DISCORD_TOKEN = "Enter Discord token here"
+USER_ID = "Enter target user ID here"
+
+# Paths
+
+BASE_PATH = "/home/user/discord-tracker-bot/" # Not required
+RESULTS_PATH = BASE_PATH + "results.csv"
+CRONFILE_PATH = BASE_PATH + "cronfile.txt"
+LOG_PATH = BASE_PATH + "log.txt" # Required only for log_message
+
+# Ping Information
+
+PINGS_PER_DAY = 10
+DAY_START = "8:00"
+DAY_END = "20:00"
+
+
+# Functions
+
+def log_message(name, message):
+
+    with open(LOG_PATH, "a") as logfile:
+        logfile.write(datetime.datetime.now() + " " + name + ": " + message + "\n")
