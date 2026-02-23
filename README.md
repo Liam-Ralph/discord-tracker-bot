7835fc373382bb65d04449717727124d46245ff5
Liam Ralph <liamralph42@gmail.com>
2026-02-22 21:40:38 -0500

    update readme, add comments to settings

--- README.md
+++ README.md
@@ -10,3 +10,27 @@ Discord Tracker Bot is a template to create a Discord bot that pings a specific
 user and records their responses. Pings a user a certain number of times each
 by sending them a DM on Discord, and records every response the user has given
 since the last ping.
+
+<br/>
+
+## Requirements
+Requires Linux, as well as rtcwake, which must also be supported by your
+hardware.
+
+<br/>
+
+## License
+This project is licensed under the MIT Licensed, also called the Expat License,
+as detailed in LICENSE.
+
+<br/>
+
+## Usage
+ - Set up your variables in `tracker_bot_settings.py`.
+ - Set up the files defined in your settings, namely `RESULTS_PATH`,
+   `CRONFILE_PATH`, `LOG_PATH`.
+ - Copy all Python files to /usr/bin/, the command `sudo cp tracker* /usr/bin/`
+   should suffice.
+ - Schdule the first run of `tracker_bot_cron.py` manually, either by running it
+   yourself before `DAY_START`, or by scheduling it with cron and booting the
+   machine with rtcwake.
--- tracker_bot_settings.py
+++ tracker_bot_settings.py
@@ -20,27 +20,34 @@ import datetime
 # Discord Token
 
 DISCORD_TOKEN = "Enter Discord token here"
-USER_ID = "Enter target user ID here"
+USER_ID = "Enter target user ID here" # The ID of the user who will be pinged
 
 # Paths
 
 BASE_PATH = "/home/user/discord-tracker-bot/" # Not required
-RESULTS_PATH = BASE_PATH + "results.csv"
-CRONFILE_PATH = BASE_PATH + "cronfile.txt"
+RESULTS_PATH = BASE_PATH + "results.csv" # Path for storing user response data
+CRONFILE_PATH = BASE_PATH + "cronfile.txt" # Path used to temporarily write cron jobs
 LOG_PATH = BASE_PATH + "log.txt" # Required only for log_message
 
 # Ping Information
 
-PINGS_PER_DAY = 10
-DAY_START = 8 * 60
-DAY_END = 20 * 60
-MIN_PING_GAP = 30
-PRE_PING_BOOT = 5
+PINGS_PER_DAY = 10 # Number of randomly timed pings per day
+DAY_START = 8 * 60 # The start of the day, in minutes since 00:00,
+                   # must be at least PRE_BOOT_PING minutes past 00:00
+DAY_END = 20 * 60 # The end of the day, in minutes since 00:00, must be at least
+                  # DAY_START + MIN_PING_GAP * PINGS_PER_DAY
+MIN_PING_GAP = 30 # Minimum number of minutes between pings, must be greater
+                  # than PRE_PING_BOOT
+PRE_PING_BOOT = 5 # Number of minutes before each ping (and DAY_START for cron
+                  # setup) to boot the machine
 
 
 # Functions
 
 def log_message(name, message):
+    """
+    
+    """
 
     text = str(datetime.datetime.now()) + " " + name + ": " + message
     print(text)
